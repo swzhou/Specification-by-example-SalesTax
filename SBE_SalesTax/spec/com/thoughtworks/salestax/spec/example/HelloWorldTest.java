085bdd919fea51026344fdc1b2859382b2fc7c3a
qszhuan <p4ssw0rd@163.com>
2012-02-27 13:30:59 +0800

    SWZhou & qszhuan setup environment for sbe

---
+++ SBE_SalesTax/spec/com/thoughtworks/salestax/spec/example/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.salestax.spec.example;
+
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: twer
+ * Date: Feb 27, 2012
+ * Time: 1:02:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldTest extends ConcordionTestCase {
+    public String getGreeting(){
+        return "Hello World";
+    }
+}
